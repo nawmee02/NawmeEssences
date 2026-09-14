71de67540c9f243b2f59d6c60f7f9685ff979c02
nawmee02 <nawmee8@gmail.com>
2026-09-15 01:24:45 +0600

    Preload product hero images

--- scripts/generate-product-pages.js
+++ scripts/generate-product-pages.js
@@ -394,6 +394,7 @@ function renderPage(p, all, detailsMap) {
   <script type="application/ld+json">${JSON.stringify(productLd)}</script>
   <script type="application/ld+json">${JSON.stringify(breadcrumbLd)}</script>
   <link rel="preconnect" href="https://cdn.nawmeessences.me" />
+  <link rel="preload" as="image" href="${attr(heroMedium(p.id, v))}" fetchpriority="high" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=Inter:wght@400;500;600;700&display=optional" media="print" onload="this.media='all'" />
